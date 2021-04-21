7cf731040fed8508689e1e1a3f428d0eba87710b
harshita kanwa <60289182+harshitakanwa@users.noreply.github.com>
2021-04-21 17:24:20 +0530

    Update SCAN.cpp

--- Disk Scheduling/SCAN.cpp
+++ Disk Scheduling/SCAN.cpp
@@ -1,65 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 int SCAN(){
-    int i,k,n,m,sum=0,h;
+    int SCAN_i,SCAN_k,SCAN_n,SCAN_m,SCAN_sum=0,SCAN_h;
     cout<<"Enter total number of tracks on the disk: ";
-    cin>>m;
+    cin>>SCAN_m;
     cout<<"Enter total number of requests: ";
-    cin>>n;
+    cin>>SCAN_n;
     cout<<"Please enter the requests!"<<endl;
-    int tmp[n];
-    vector <int> a(n),b;
-    for(i=0;i<n;i++){
-        cout<<"Enter the request "<<i+1<<": ";
-        cin>>a[i];
-        if(a[i]>=m)
+    int tmp[SCAN_n];
+    vector <int> a(SCAN_n),b;
+    for(SCAN_i=0;SCAN_i<SCAN_n;SCAN_i++){
+        cout<<"Enter the request "<<SCAN_i+1<<": ";
+        cin>>a[SCAN_i];
+        if(a[SCAN_i]>=SCAN_m)
         {
-             cout<<"Error! Unknown position of track "<<a[i]<<" on the disk \n";
+             cout<<"Error! Unknown position of track "<<a[SCAN_i]<<" on the disk \n";
         }
     }
     cout<<"Enter read/write head position: ";
-    cin>>h;
-    int temp=h;
-    a.push_back(h);
-    a.push_back(m-1);
+    cin>>SCAN_h;
+    int temp=SCAN_h;
+    a.push_back(SCAN_h);
+    a.push_back(SCAN_m-1);
     a.push_back(0);
   sort(a.begin(),a.end());
     cout<<"\n";
-    for(i=0;i<a.size();i++){
-        if(h==a[i])
+    for(SCAN_i=0;SCAN_i<a.size();SCAN_i++){
+        if(SCAN_h==a[SCAN_i])
             break;
     }
-    k=i;
-    if(k<n/2){
-        for(i=k;i<a.size();i++){
-            b.push_back(a[i]);
+    SCAN_k=SCAN_i;
+    if(SCAN_k<SCAN_n/2){
+        for(SCAN_i=SCAN_k;SCAN_i<a.size();SCAN_i++){
+            b.push_back(a[SCAN_i]);
         }
-        for(i=k-1;i>=0;i--){
-            b.push_back(a[i]);
+        for(SCAN_i=SCAN_k-1;SCAN_i>=0;SCAN_i--){
+            b.push_back(a[SCAN_i]);
         }
         cout<<"Here,Read/Write head is moving towards right side and reverses its direction afterwards!"<<endl;
     }
     else{
-        for(i=k;i>=0;i--){
-            b.push_back(a[i]);
+        for(SCAN_i=SCAN_k;SCAN_i>=0;SCAN_i--){
+            b.push_back(a[SCAN_i]);
         }
-        for(i=k+1;i<a.size();i++){
-            b.push_back(a[i]);
+        for(SCAN_i=SCAN_k+1;SCAN_i<a.size();SCAN_i++){
+            b.push_back(a[SCAN_i]);
         }
         cout<<"Here,Read/Write head is moving towards left side and reverses its direction afterwards!"<<endl;
     }
     temp=b[0];
     cout<<temp;
-    for(i=1;i<b.size();i++){
-            if(b[i]>=m)
+    for(SCAN_i=1;SCAN_i<b.size();SCAN_i++){
+            if(b[SCAN_i]>=SCAN_m)
             continue;
-        cout<<" -> "<<b[i];
+        cout<<" -> "<<b[SCAN_i];
         // calculating the difference for the read/write head movement
-        sum+=abs(b[i]-temp);
+        SCAN_sum+=abs(b[SCAN_i]-temp);
         // head is now at the next I/O request
-        temp=a[i];
+        temp=a[SCAN_i];
     }
     cout<<"\n";
-    cout<<"Total seek time = "<< sum<<"\n";
+    cout<<"Total seek time = "<< SCAN_sum<<"\n";
     return 0;
 }
+       
