80628b49cbdee86736cf5dbd9ecd428e05dabb62
harshita kanwa <60289182+harshitakanwa@users.noreply.github.com>
2021-04-21 17:31:55 +0530

    Update CSCAN.cpp

--- Disk Scheduling/CSCAN.cpp
+++ Disk Scheduling/CSCAN.cpp
@@ -1,51 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 int CSCAN(){
-    int i,k,n,m,sum=0,h;
+    int CSCAN_i,CSCAN_k,CSCAN_n,CSCAN_m,CSCAN_sum=0,CSCAN_h;
     cout<<"Enter total number of tracks on the disk: ";
-    cin>>m;
+    cin>>CSCAN_m;
     cout<<"Enter total number of requests: ";
-    cin>>n;
+    cin>>CSCAN_n;
     cout<<"Please enter the requests!"<<endl;
-    vector <int> a(n),b;
-    for(i=0;i<n;i++){
-        cout<<"Enter the request "<<i+1<<": ";
-        cin>>a[i];
-        if(a[i]>=m)
+    vector <int> a(CSCAN_n),b;
+    for(CSCAN_i=0;CSCAN_i<CSCAN_n;CSCAN_i++){
+        cout<<"Enter the request "<<CSCAN_i+1<<": ";
+        cin>>a[CSCAN_i];
+        if(a[CSCAN_i]>=CSCAN_m)
         {
-             cout<<"Error! Unknown position of track "<<a[i]<<" on the disk \n";
+             cout<<"Error! Unknown position of track "<<a[CSCAN_i]<<" on the disk \n";
         }
 
     }
     cout<<"Enter read/write head position: ";
-    cin>>h;
-    int temp=h;
-    a.push_back(h);
-    a.push_back(m-1);
+    cin>>CSCAN_h;
+    int temp=CSCAN_h;
+    a.push_back(CSCAN_h);
+    a.push_back(CSCAN_m-1);
     a.push_back(0);
     sort(a.begin(),a.end());
-    for(i=0;i<a.size();i++){
-        if(h==a[i])
+    for(CSCAN_i=0;CSCAN_i<a.size();CSCAN_i++){
+        if(CSCAN_h==a[CSCAN_i])
             break;
     }
-    k=i;
-    if(k<n/2){
-        for(i=k;i<a.size();i++){
-            b.push_back(a[i]);
+    CSCAN_k=CSCAN_i;
+    if(CSCAN_k<CSCAN_n/2){
+        for(CSCAN_i=CSCAN_k;CSCAN_i<a.size();CSCAN_i++){
+            b.push_back(a[CSCAN_i]);
         }
-        for(i=0;i<=k-1;i++){
-            b.push_back(a[i]);
+        for(CSCAN_i=0;CSCAN_i<=CSCAN_k-1;CSCAN_i++){
+            b.push_back(a[CSCAN_i]);
         }
         //"Here,Read/Write head is moving towards right side(services requests on its way) , reaches "0"  and goes to the extreme position
         // then reverses its direction(services requests on its way).
          cout<<"Here,Read/Write head is moving towards right side and reverses its direction afterwards!"<<endl;
     }
     else{
-        for(i=k;i>=0;i--){
-            b.push_back(a[i]);
+        for(CSCAN_i=CSCAN_k;CSCAN_i>=0;CSCAN_i--){
+            b.push_back(a[CSCAN_i]);
         }
-        for(i=a.size()-1;i>=k+1;i--){
-            b.push_back(a[i]);
+        for(CSCAN_i=a.size()-1;CSCAN_i>=CSCAN_k+1;CSCAN_i--){
+            b.push_back(a[CSCAN_i]);
         }
         //"Here,Read/Write head is moving towards left side(services requests on its way) , reaches "0"  and goes to the extreme position
         // then reverses its direction(services requests on its way).
@@ -53,16 +53,17 @@ int CSCAN(){
     }
     temp=b[0];   //head h is at the beginning of vector b
     cout<<temp;
-    for(i=1;i<b.size();i++){
-            if(b[i]>=m)
+    for(CSCAN_i=1;CSCAN_i<b.size();CSCAN_i++){
+            if(b[CSCAN_i]>=CSCAN_m)
             continue;
-        cout<<" -> "<<b[i];
+        cout<<" -> "<<b[CSCAN_i];
         // calculating the difference for the read/write head movement
-        sum+=abs(b[i]-temp);
+        CSCAN_sum+=abs(b[CSCAN_i]-temp);
         // head is now at the next I/O request
-        temp=b[i];
+        temp=b[CSCAN_i];
     }
     cout<<"\n";
-    cout<<"Total seek time =  "<< sum<<"\n";
+    cout<<"Total seek time =  "<< CSCAN_sum<<"\n";
     return 0;
 }
+           
