451a84dfdb5eb69d1a8f43d11357834864da6247
harshita kanwa <60289182+harshitakanwa@users.noreply.github.com>
2021-04-21 17:35:27 +0530

    Update FIFO.cpp

--- Disk Scheduling/FIFO.cpp
+++ Disk Scheduling/FIFO.cpp
@@ -1,37 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 int FIFO(){
-    int i,j,k,n,m,sum=0,x,y,h;
+    int FIFO_i,FIFO_j,FIFO_k,FIFO_n,FIFO_m,FIFO_sum=0,FIFO_x,FIFO_y,FIFO_h;
     cout<<"Enter total number of tracks on the disk: ";
-    cin>>m;
+    cin>>FIFO_m;
     cout<<"Enter total number of requests: ";
-    cin>>n;
+    cin>>FIFO_n;
     cout<<"Please enter the requests!"<<endl;
     //a vector of size n
-    vector <int> a(n);
-    for(i=0;i<n;i++){
-        cout<<"Enter the request "<<i+1<<": ";
-        cin>>a[i];
-        if(a[i]>=m)
+    vector <int> a(FIFO_n);
+    for(FIFO_i=0;FIFO_i<FIFO_n;FIFO_i++){
+        cout<<"Enter the request "<<FIFO_i+1<<": ";
+        cin>>a[FIFO_i];
+        if(a[FIFO_i]>=FIFO_m)
         {
-             cout<<"Error! Unknown position of track "<<a[i]<<" on the disk \n";
+             cout<<"Error! Unknown position of track "<<a[FIFO_i]<<" on the disk \n";
         }
     }
     cout<<"Enter  read/write head position: ";
-    cin>>h;
+    cin>>FIFO_h;
     // head position is at h in the beginning
-    int temp=h;
+    int temp=FIFO_h;
     cout<<temp;
-    for(i=0;i<n;i++){
-            if(a[i]>=m)
+    for(FIFO_i=0;FIFO_i<FIFO_n;FIFO_i++){
+            if(a[FIFO_i]>=FIFO_m)
             continue;
-        cout<<" -> "<<a[i]<<' ';
+        cout<<" -> "<<a[FIFO_i]<<' ';
         // calculating the difference for the read/write head movement
-        sum+=abs(a[i]-temp);
+        FIFO_sum+=abs(a[FIFO_i]-temp);
         // head is now at the next I/O request
-        temp=a[i];
+        temp=a[FIFO_i];
     }
     cout<<'\n';
-    cout<<"Total seek time = "<< sum<<"\n";
+    cout<<"Total seek time = "<< FIFO_sum<<"\n";
     return 0;
 }
