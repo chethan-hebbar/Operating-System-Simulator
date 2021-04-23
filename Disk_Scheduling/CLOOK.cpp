646c20d6a412986d7bca05fe84468594933624d9
ashueep <ashuanand29@gmail.com>
2021-04-23 12:34:23 +0400

    change

--- Disk_Scheduling/CLOOK.cpp
+++ Disk_Scheduling/CLOOK.cpp
@@ -13,66 +13,75 @@
 #include <climits>
 #include <cstring>
 using namespace std;
-int CLOOK(){
-    int clook_i,clook_k,clook_n,clook_m,clook_sum=0,clook_h;
-     cout<<"Enter total number of tracks on the disk: ";
-    cin>>clook_m;
-    cout<<"Enter total number of requests: ";
-    cin>>clook_n;
-    cout<<"Please enter the requests!"<<endl;
-    vector <int> a(clook_n),l;
-    for(clook_i=0;clook_i<clook_n;clook_i++){
-        cout<<"Enter the request "<<clook_i+1<<": ";
-        cin>>a[clook_i];
-        if(a[clook_i]>=clook_m)
+int CLOOK()
+{
+    int clook_i, clook_k, clook_n, clook_m, clook_sum = 0, clook_h;
+    cout << "Enter total number of tracks on the disk: ";
+    cin >> clook_m;
+    cout << "Enter total number of requests: ";
+    cin >> clook_n;
+    cout << "Please enter the requests!" << endl;
+    vector<int> a(clook_n), l;
+    for (clook_i = 0; clook_i < clook_n; clook_i++)
+    {
+        cout << "Enter the request " << clook_i + 1 << ": ";
+        cin >> a[clook_i];
+        if (a[clook_i] >= clook_m)
         {
-             cout<<"Error! Unknown position of track "<<a[clook_i]<<" on the disk \n";
+            cout << "Error! Unknown position of track " << a[clook_i] << " on the disk \n";
         }
-
     }
-    cout<<"Enter read/write head position: ";
-    cin>>clook_h;
+    cout << "Enter read/write head position: ";
+    cin >> clook_h;
     a.push_back(clook_h);
-    sort(a.begin(),a.end());
-    for(clook_i=0;clook_i<a.size();clook_i++){
-        if(clook_h==a[clook_i])
+    sort(a.begin(), a.end());
+    for (clook_i = 0; clook_i < a.size(); clook_i++)
+    {
+        if (clook_h == a[clook_i])
             break;
     }
-    clook_k=clook_i;
-    if(clook_k<clook_n/2){
-        for(clook_i=clook_k;clook_i<a.size();clook_i++){
+    clook_k = clook_i;
+    if (clook_k < clook_n / 2)
+    {
+        for (clook_i = clook_k; clook_i < a.size(); clook_i++)
+        {
             l.push_back(a[clook_i]);
         }
-        for(clook_i=0;clook_i<clook_k;clook_i++){
+        for (clook_i = 0; clook_i < clook_k; clook_i++)
+        {
             l.push_back(a[clook_i]);
         }
         //"Here,Read/Write head is moving towards right side(services requests on its way) , reaches "0"  and goes to the extreme position
         // then reverses its direction(services requests on its way).
-         cout<<"Here,Read/Write head is moving towards right side and reverses its direction afterwards!"<<endl;
+        cout << "Here,Read/Write head is moving towards right side and reverses its direction afterwards!" << endl;
     }
-    else{
-        for(clook_i=clook_k;clook_i>=0;clook_i--){
+    else
+    {
+        for (clook_i = clook_k; clook_i >= 0; clook_i--)
+        {
             l.push_back(a[clook_i]);
         }
-        for(clook_i=a.size()-1;clook_i>clook_k;clook_i--){
+        for (clook_i = a.size() - 1; clook_i > clook_k; clook_i--)
+        {
             l.push_back(a[clook_i]);
         }
         //"Here,Read/Write head is moving towards left side(services requests on its way) , reaches "0"  and goes to the extreme position
         // then reverses its direction(services requests on its way).
-         cout<<"Here,Read/Write head is moving towards left side and reverses its direction afterwards!"<<endl;
+        cout << "Here,Read/Write head is moving towards left side and reverses its direction afterwards!" << endl;
     }
-    int temp=l[0];
-    cout<<temp;
-    for(clook_i=1;clook_i< l.size();clook_i++){
-            if(l[clook_i]>=clook_m)
+    int temp = l[0];
+    cout << temp;
+    for (clook_i = 1; clook_i < l.size(); clook_i++)
+    {
+        if (l[clook_i] >= clook_m)
             continue;
-        cout<<" -> "<<l[clook_i];
-       // calculating the difference for the read/write head movement
-        clook_sum+=abs(l[clook_i]-temp);
+        cout << " -> " << l[clook_i];
+        // calculating the difference for the read/write head movement
+        clook_sum += abs(l[clook_i] - temp);
         // head is now at the next I/O request
-        temp=a[clook_i];
+        temp = l[clook_i];
     }
-    cout<<"\n";
-    cout<<"Total seek time = "<< clook_sum<<"\n";
+    cout << "\n";
+    cout << "Total seek time = " << clook_sum << "\n";
     return 0;
 }
--- Disk_Scheduling/SCAN.cpp
+++ Disk_Scheduling/SCAN.cpp
@@ -13,67 +13,80 @@
 #include <climits>
 #include <cstring>
 using namespace std;
-int SCAN(){
-    int SCAN_i,SCAN_k,SCAN_n,SCAN_m,SCAN_sum=0,SCAN_h;
-    cout<<"Enter total number of tracks on the disk: ";
-    cin>>SCAN_m;
-    cout<<"Enter total number of requests: ";
-    cin>>SCAN_n;
-    cout<<"Please enter the requests!"<<endl;
-    int tmp[SCAN_n];
-    vector <int> a(SCAN_n),b;
-    for(SCAN_i=0;SCAN_i<SCAN_n;SCAN_i++){
-        cout<<"Enter the request "<<SCAN_i+1<<": ";
-        cin>>a[SCAN_i];
-        if(a[SCAN_i]>=SCAN_m)
+int SCAN()
+{
+    int i, k, n, m, sum = 0, h;
+    cout << "Enter total number of tracks on the disk: ";
+    cin >> m;
+    cout << "Enter total number of requests: ";
+    cin >> n;
+    cout << "Please enter the requests!" << endl;
+    int tmp[n];
+    vector<int> a(n), b;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter the request " << i + 1 << ": ";
+        cin >> a[i];
+        if (a[i] >= m)
         {
-             cout<<"Error! Unknown position of track "<<a[SCAN_i]<<" on the disk \n";
+            cout << "Error! Unknown position of track " << a[i] << " on the disk \n";
         }
     }
-    cout<<"Enter read/write head position: ";
-    cin>>SCAN_h;
-    int temp=SCAN_h;
-    a.push_back(SCAN_h);
-    a.push_back(SCAN_m-1);
+    cout << "Enter read/write head position: ";
+    cin >> h;
+    int temp = h;
+    a.push_back(h);
+    // a.push_back(m - 1);
     a.push_back(0);
-  sort(a.begin(),a.end());
-    cout<<"\n";
-    for(SCAN_i=0;SCAN_i<a.size();SCAN_i++){
-        if(SCAN_h==a[SCAN_i])
+    sort(a.begin(), a.end());
+    cout << "\n";
+    for (i = 0; i < a.size(); i++)
+    {
+        if (h == a[i])
             break;
     }
-    SCAN_k=SCAN_i;
-    if(SCAN_k<SCAN_n/2){
-        for(SCAN_i=SCAN_k;SCAN_i<a.size();SCAN_i++){
-            b.push_back(a[SCAN_i]);
+    k = i;
+    if (k < n / 2)
+    {
+        for (i = k; i < a.size(); i++)
+        {
+            b.push_back(a[i]);
         }
-        for(SCAN_i=SCAN_k-1;SCAN_i>=0;SCAN_i--){
-            b.push_back(a[SCAN_i]);
+        for (i = k - 1; i >= 0; i--)
+        {
+            b.push_back(a[i]);
         }
-        cout<<"Here,Read/Write head is moving towards right side and reverses its direction afterwards!"<<endl;
+        cout << "Here,Read/Write head is moving towards right side and reverses its direction afterwards!" << endl;
     }
-    else{
-        for(SCAN_i=SCAN_k;SCAN_i>=0;SCAN_i--){
-            b.push_back(a[SCAN_i]);
+    else
+    {
+        for (i = k; i >= 0; i--)
+        {
+            b.push_back(a[i]);
         }
-        for(SCAN_i=SCAN_k+1;SCAN_i<a.size();SCAN_i++){
-            b.push_back(a[SCAN_i]);
+        for (i = k + 1; i < a.size(); i++)
+        {
+            b.push_back(a[i]);
         }
-        cout<<"Here,Read/Write head is moving towards left side and reverses its direction afterwards!"<<endl;
+        cout << "Here,Read/Write head is moving towards left side and reverses its direction afterwards!" << endl;
     }
-    temp=b[0];
-    cout<<temp;
-    for(SCAN_i=1;SCAN_i<b.size();SCAN_i++){
-            if(b[SCAN_i]>=SCAN_m)
+    temp = b[0];
+    cout << temp;
+    for (i = 1; i < b.size(); i++)
+    {
+        if (b[i] >= m)
             continue;
-        cout<<" -> "<<b[SCAN_i];
+        cout << " -> " << b[i];
         // calculating the difference for the read/write head movement
-        SCAN_sum+=abs(b[SCAN_i]-temp);
-        // head is now at the next I/O request
-        temp=a[SCAN_i];
+        if (b[i] >= m)
+            continue;
+
+        sum += abs(b[i] - temp);
+        if (b[i] >= m)
+            continue;
+        temp = b[i];
     }
-    cout<<"\n";
-    cout<<"Total seek time = "<< SCAN_sum<<"\n";
+    cout << "\n";
+    cout << "Total seek time = " << sum << "\n";
     return 0;
 }
-       
